a59580ce2cb6f4dd93ce320a4ebd0f2e645922c7
okamuuu <okamuuuuu@gmail.com>
2010-12-13 22:08:32 +0900

    refactoring

--- lib/Test/Groonga.pm
+++ lib/Test/Groonga.pm
@@ -5,21 +5,21 @@ use File::Spec ();
 use File::Temp ();
 use IO::Socket::INET;
 use Time::HiRes ();
-use Class::Accessor::Lite;
-Class::Accessor::Lite->mk_accessors(qw/pid port protocol host temp_db/);
+use Class::Accessor::Lite ( 
+    ro => [qw/host protocol temp_db/],
+    rw => [qw/port/]
+);
 
-use Data::Dumper;
-
-our $VERSION = '0.00001';
+our $VERSION = '0.01';
 
 sub new {
     my $class = shift;
     my %args = @_ == 1 ? %{ $_[0] } : @_;
 
     bless {
-        port    => undef,
-        host    => 'localhost',
-        temp_db => File::Spec->catfile(
+        host     => 'localhost',
+        protocol => $args{protocol} || undef,
+        temp_db  => File::Spec->catfile(
             File::Temp::tempdir( CLEANUP => 1 ),
             'test.groonga.db'
         ),
@@ -29,44 +29,49 @@ sub new {
 sub start {
     my $self = shift;
 
+    Carp::croak('Already running...') if $self->is_running;
+
     my $groonga_cmd = $self->which_groonga_cmd;
-    my $port        = $self->port || 19000;
     my $host        = $self->host; 
     my $temp_db     = $self->temp_db;
 
-    if ( $port !~ m/^[0-9]+/ or $port < 19000 ) {
-        $port = 19000;
-    }
-   
-    my $sock;
-    while ( $port++ < 20000 ) {
-        $sock = IO::Socket::INET->new(
-            Listen    => 5,
-            LocalAddr => '127.0.0.1',
-            LocalPort => $port,
-            Proto     => 'tcp',
-            (($^O eq 'MSWin32') ? () : (ReuseAddr => 1)),
-        );
-        last if $sock;
-    }
-    if (! $sock) {
-        die "empty port not found";
-    }
-    $sock->close; 
- 
+    my $port = $self->get_empty_port();
     $self->port($port);
 
-    `$groonga_cmd -p $port -d -n $temp_db`;
+    my $cmd =
+      ( $self->protocol and $self->protocol eq 'http' )
+      ? "$groonga_cmd -p $port --protocol http -d -n $temp_db 2> /dev/null"
+      : "$groonga_cmd -p $port                 -d -n $temp_db 2> /dev/null";
 
-    ### TODO: catch and hide errstr
-    my $retry = 100;
-    while ( $retry-- ) {
-        ### wating groonga worked.
-        return if `$groonga_cmd -p $port -c $host status`;
+    system($cmd);
+
+    for ( 0 .. 100 ) {
+        return if $self->is_running;
         Time::HiRes::sleep(0.1);
     }
     
-    die "cannot open port: $port";
+    die "cannot running groonga :(";
+}
+
+sub is_running {
+    my $self = shift;
+
+    my $port = $self->port or return 0;
+
+    my $groonga_cmd = $self->which_groonga_cmd;
+    my $host        = $self->host;
+
+    if ( $self->protocol and $self->protocol eq 'http' ) {
+        require LWP::Simple;
+        ### XXX: LWP::Simple::head is balkiness :( 
+        my $content = LWP::Simple::get("http://localhost:$port/d/status");
+        return $content ? 1 : 0;
+    }
+    else {
+        ### BK: SEE ALSO https://github.com/schwern/test-more/issues/issue/83
+        local $?;
+        return `$groonga_cmd -p $port -c $host status 2> /dev/null` ? 1 : 0;
+    }
 }
 
 sub stop {
@@ -76,9 +81,15 @@ sub stop {
     my $port        = $self->port;
     my $temp_db     = $self->temp_db;
     my $host        = $self->host;
-
-    `$groonga_cmd -p $port -c $host shutdown`
-      or die "failed to shutdowon groonga daemon...";
+    
+    if ( $self->protocol and $self->protocol eq 'http' ) {
+        my $content = LWP::Simple::get("http://localhost:$port/d/shutdown");
+        die "failed to shutdowon groonga daemon..." unless $content;
+    }
+    else {
+        my $result = `$groonga_cmd -p $port -c $host shutdown 2> /dev/null`;
+        die "failed to shutdowon groonga daemon..." unless $result;
+    }
 }
 
 sub can_groonga_cmd {
@@ -91,25 +102,30 @@ sub which_groonga_cmd {
     return $path;
 }
 
-=pod
+sub get_empty_port {
+    my $self = shift;
+
+    my $port = 19000; 
+    my $sock;
+    while ( $port++ < 20000 ) {
+        $sock = IO::Socket::INET->new(
+            Listen    => 5,
+            LocalAddr => '127.0.0.1',
+            LocalPort => $port,
+            Proto     => 'tcp',
+        );
+        last if $sock;
+    }
+    die "empty port not found" unless $sock;
+
+    $sock->close; 
+    return $port;
+}
+
 sub DESTROY {
     my $self = shift;
-    $self->stop
-        if defined $self->pid && $$ == $self->_owner_pid;
+    $self->stop if $self->is_running;
 }
-
-sub start {
-
-    $sock = IO::Socket::INET->new(
-        Listen    => 5,
-        LocalAddr => '127.0.0.1',
-        LocalPort => $port,
-        Proto     => 'tcp',
-        ( ( $^O eq 'MSWin32' ) ? () : ( ReuseAddr => 1 ) ),
-    );
-
-}
-=cut
 
 1;
 
@@ -117,21 +133,27 @@ __END__
 
 =head1 NAME
 
-Test::Groonga::Httpd -
+Test::Groonga -  Groonga Runner For Tests
 
 =head1 SYNOPSIS
 
-  use Test::Groonga::Httpd;
+  use Test::Groonga;
+ 
+  my $groonga = Test::Groonga->new();
+  
+  $groonga->start;
+  $groonga->stop;
 
 =head1 DESCRIPTION
 
-Test::Groonga::Httpd is
+Test::Groonga provides you temporary groonga server daemon.
+
+I also shamelessly stole from Test::Memcached.
+So this interface similar to it.
 
 =head1 AUTHOR
 
-okamuuu E<lt>okamuuu@gmail.comE<gt>
-
-=head1 SEE ALSO
+Okamura. 
 
 =head1 LICENSE
 
@@ -139,3 +161,5 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
+
--- t/test_groonga.t
+++ t/test_groonga.t
@@ -1,18 +1,27 @@
 use strict;
 use warnings;
+use Carp ();
 use Test::More;
 use Test::Exception;
-use LWP::Simple;
-use JSON;
+
+### TODO: Where is the BEST PRACTICE How to do this
+if ( $Class::Accessor::Lite::VERSION < 0.04 ) {
+    Carp::croak "Test::Groonga use read-only accessor "
+      . "via Class::Accessor::Lite newer than version 0.04 ...";
+}
 
 BEGIN { use_ok 'Test::Groonga' }
 
-subtest 'test groonga utility methods.' => sub {
+subtest 'test groonga utility CLASS METHODS.' => sub {
 
     my $path = Test::Groonga->which_groonga_cmd;
     ok( $path, "I find groonga command: $path" );
+
     ok( Test::Groonga->can_groonga_cmd,
         'And it is executable.' );
+    
+    my $port = Test::Groonga->get_empty_port;
+    ok $port, "get empty port ok: $port";
 };
 
 subtest 'create instance.' => sub {
@@ -20,13 +29,33 @@ subtest 'create instance.' => sub {
     my $groonga = Test::Groonga->new();
     isa_ok( $groonga, "Test::Groonga" );
 
-    ok ! $groonga->port,  "This case I don't spcified port.";
+    ok ! $groonga->port,  "Still not set port.";
     ok $groonga->temp_db, "This instance has temporary file: @{[$groonga->temp_db]}";
-
-#    lives_ok { $groonga->start } 'Now start groonga daemon';
-    $groonga->start;
-    $groonga->stop;
+    
+    ok ! $groonga->is_running;
+    lives_ok { $groonga->start } 'start groonga daemon.';
+    ok $groonga->is_running;
+    lives_ok { $groonga->stop  } 'stop groonga daemon.';
+    ok ! $groonga->is_running;
 };
 
+subtest 'create instance as httpd.' => sub {
+
+    my $groonga = Test::Groonga->new(protocol => 'http');
+    isa_ok( $groonga, "Test::Groonga" );
+
+    ok ! $groonga->port,  "Still not set port.";
+    ok $groonga->temp_db, "This instance has temporary file: @{[$groonga->temp_db]}";
+    
+    ok ! $groonga->is_running;
+    lives_ok { $groonga->start } 'start groonga daemon.';
+    ok $groonga->is_running;
+    lives_ok { $groonga->stop  } 'stop groonga daemon.';
+    ok ! $groonga->is_running;
+};
+
+
 done_testing;
+
+
 
